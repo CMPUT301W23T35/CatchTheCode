cc36d1c195091fdbf44a9971b02a02328b9e37c5
JohnsonWu1 <wjc2848949342@gmail.com>
2023-03-03 11:45:08 -0700

    remove extra xmls

--- CatchTheCode/app/src/main/java/com/example/catchthecode/MainActivity.java
+++ CatchTheCode/app/src/main/java/com/example/catchthecode/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
 
 
         // This line is needed in every function to access database
