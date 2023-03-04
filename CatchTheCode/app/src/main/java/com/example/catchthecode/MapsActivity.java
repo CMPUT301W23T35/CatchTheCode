a621b3b9ad6c516e7366e90e4ef5a144220ac850
JohnsonWu1 <wjc2848949342@gmail.com>
2023-03-03 23:39:15 -0700

    map updated

--- CatchTheCode/app/src/main/java/com/example/catchthecode/MapsActivity.java
+++ CatchTheCode/app/src/main/java/com/example/catchthecode/MapsActivity.java
@@ -1,12 +1,25 @@
 package com.example.catchthecode;
 
+import static java.security.AccessController.getContext;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.SensorEventListener;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -14,10 +27,13 @@ import com.example.catchthecode.databinding.ActivityMapsBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private LocationManager locationManager;
     FirebaseFirestore db;
 
     @Override
@@ -37,10 +53,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng ualberta = new LatLng(5.3521331248E01,- 1.13521331248E02);
-        mMap.addMarker(new MarkerOptions().position(ualberta).title("Marker in University of Alberta"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ualberta));
+        // try to get the location permission
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // add a marker to the current location
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
 
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        android.location.Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+        double userLat = lastKnownLocation.getLatitude();
+        double userLong = lastKnownLocation.getLongitude();
+        Log.d("userLat", String.valueOf(userLat));
+        Log.d("userLong", String.valueOf(userLong));
+        LatLng userLocation = new LatLng(userLat, userLong);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+        // add all QR codes stored in the database to the map
+        // addAllQRs(mMap);
     }
 
     // add all QR codes stored in the database to the map
