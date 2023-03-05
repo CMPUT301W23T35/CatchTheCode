ab4934a9e850658cf830ddb5685db35be1d9a788
JohnsonWu1 <wjc2848949342@gmail.com>
2023-03-04 17:32:17 -0700

    change the button usage

--- CatchTheCode/app/src/main/java/com/example/catchthecode/UserActivity.java
+++ CatchTheCode/app/src/main/java/com/example/catchthecode/UserActivity.java
@@ -7,10 +7,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FriendActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.friend_page);
+        setContentView(R.layout.user_page);
     }
 }
