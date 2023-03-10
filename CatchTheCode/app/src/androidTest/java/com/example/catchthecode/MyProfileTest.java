02289fddb52427b44f0f765e807e8e4197cb81d7
Yujie Wen <yujiewen911@gmail.com>
2023-03-10 14:53:22 -0700

    Added tests for most of models/controllers

---
+++ CatchTheCode/app/src/androidTest/java/com/example/catchthecode/MyProfileTest.java
@@ -0,0 +1,67 @@
+package com.example.catchthecode;
+import android.app.Activity;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.robotium.solo.Solo;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+
+public class MyProfileTest {
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void testModify(){
+        solo.clickOnButton("my profile");
+        solo.assertCurrentActivity("Wrong activity",UserActivity.class);
+        solo.clickOnButton("Modify Profile");
+        solo.enterText((EditText) solo.getView(R.id.contactInfoText), "123333");
+        solo.enterText((EditText) solo.getView(R.id.enterUserName), "Jason");
+
+
+        Button positiveButton = solo.getButton("Save");
+        assertNotNull("Positive button not found", positiveButton);
+        solo.clickOnView(positiveButton);
+
+        TextView name = (TextView) solo.getView(R.id.playerID);
+        TextView contact = (TextView) solo.getView(R.id.info);
+
+
+        assertEquals(name.getText().toString(),"Jason");
+        assertEquals(contact.getText().toString(),"Phone number is123333");
+    }
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+
+}
