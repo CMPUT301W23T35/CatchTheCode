af80e8de36764aed36469eff45b5e8390b15abbb
Yujie Wen <yujiewen911@gmail.com>
2023-03-21 21:28:11 -0600

    Added support for collection page

--- CatchTheCode/app/src/main/java/com/example/catchthecode/CollectionActivity.java
+++ CatchTheCode/app/src/main/java/com/example/catchthecode/CollectionActivity.java
@@ -3,7 +3,14 @@ package com.example.catchthecode;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 /**
  * This class defines the collection page of the app.
@@ -20,5 +27,32 @@ public class CollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collection_page);
+
+        TextView rankingNum = findViewById(R.id.current_ranking_nums);
+        TextView highest = findViewById(R.id.highest_button_num);
+        TextView lowest = findViewById(R.id.lowest_button_num);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        // Set ranking
+        db.collection("users")
+                .orderBy("highest", Query.Direction.DESCENDING)
+                .get()
+                .addOnCompleteListener( task -> {
+                    int rankCounter = 1;
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if (document.getId().equals(androidId)) {
+                            // Found the user, so output their ranking
+                            highest.setText(String.valueOf(document.getLong("highest")));
+                            lowest.setText(String.valueOf(document.getLong("lowest")));
+                            break;
+                        } else {
+                            // Increment the ranking for each user that has a higher highest value than the current user
+                            rankCounter++;
+                        }
+                    }
+                    rankingNum.setText(String.valueOf(rankCounter));
+                });
+
     }
 }
