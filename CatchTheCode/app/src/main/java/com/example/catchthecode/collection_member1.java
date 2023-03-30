022435c1ce7bf0be22be2097ffa9d7a3fc81f652
MingbaoF <mingbao@ualberta.ca>
2023-03-30 00:02:49 -0600

    collection_comment_member.xml added

---
+++ CatchTheCode/app/src/main/java/com/example/catchthecode/collection_member1.java
@@ -0,0 +1,17 @@
+
+package com.example.catchthecode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class collection_member1 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collection_comment_member);
+    }
+}
