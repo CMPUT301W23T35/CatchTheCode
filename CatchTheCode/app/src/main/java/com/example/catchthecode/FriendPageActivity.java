e3bb11801ac40246154d513918ff40dd30ef0ac5
Yujie Wen <yujiewen911@gmail.com>
2023-03-29 19:35:36 -0600

    Added functionality for search_friend

--- CatchTheCode/app/src/main/java/com/example/catchthecode/FriendPageActivity.java
+++ CatchTheCode/app/src/main/java/com/example/catchthecode/FriendPageActivity.java
@@ -2,9 +2,13 @@ package com.example.catchthecode;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FriendPageActivity extends AppCompatActivity {
 
@@ -14,10 +18,22 @@ public class FriendPageActivity extends AppCompatActivity {
         setContentView(R.layout.user_page);
 
         // Get the user ID from the intent
-        String userId = getIntent().getStringExtra("userId");
-
-
-
+        String userId = getIntent().getStringExtra("userid");
+        // Make
+        TextView id = findViewById(R.id.playerID);
+        TextView info = findViewById(R.id.info);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .whereEqualTo("userid", userId)
+                .get()
+                .addOnSuccessListener(querySnapshot -> {
+                    // User must exist in database. Because
+                    DocumentSnapshot documentSnapshot = querySnapshot.getDocuments().get(0);
+                    String name = documentSnapshot.getString("username");
+                    String contactInfo = documentSnapshot.getString("contactInfo");
+                    id.setText(name);
+                    info.setText("Phone number is"+contactInfo);
+                });
     }
 }
 
