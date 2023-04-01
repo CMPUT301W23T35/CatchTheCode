cce2bb304b9ccf0c71e3df3ddab24b2fb2b8ddc0
MingbaoF <mingbao@ualberta.ca>
2023-04-01 02:02:33 -0600

    loading bar created

---
+++ CatchTheCode/app/src/main/java/com/example/catchthecode/SplashActivity.java
@@ -0,0 +1,42 @@
+/**
+ The SplashActivity class is responsible for showing a splash screen for 3 seconds before
+ launching the CollectionActivity.
+ */
+package com.example.catchthecode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler handler;
+
+    /**
+     * This method is called when the SplashActivity is first created. It sets the layout for the
+     * activity and initializes the handler to launch the CollectionActivity after 3 seconds.
+     *
+     * @param savedInstanceState The saved instance state bundle, which is null if the activity
+     *                           is being launched for the first time.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            /**
+             * This method is called after the delay specified in postDelayed() has elapsed. It
+             * launches the CollectionActivity and finishes the SplashActivity.
+             */
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, CollectionActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000); // Delay for 1 seconds before launching CollectionActivity
+    }
+}
